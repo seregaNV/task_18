20f214b8bcc9077c4bce87ead7be583562274616
seregaNV <serega.n.v333@gmail.com>
2015-11-17 23:52:20 +0200

    Замінив назви пєрємєнних "json" на "companyData", і "countJson" на "countData". Переніс файл "task.json" в папку "data". Убрав "now = new Date()" і всі його елементи.

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var app = express();
-var json = require('../public/task.json');
+var companyData = require('../data/task.json');
 var util = require('util');
-var now = new Date();
 
 router.get('/', function(req, res, next) {
     res.render('index', {
-        title: 'Task 17',
-        message: 'Hello everyone!!!',
-        date: now
+        title: 'Task 17'
     });
 });
 
 router.get('/company', function(req, res, next) {
     var count = req.query.count,
-        countJson = json.slice(0, count);
+        countData = companyData.slice(0, count);
     //    page = req.query.page;
     //console.log(count);
     //console.log(page);
-    if ((!count || ((count == parseInt(Math.abs(count))) && count <= json.length)) && count != 0) {
+    if ((!count || ((count == parseInt(Math.abs(count))) && count <= companyData.length)) && count != 0) {
         res.render('all', {
             title: 'Company list',
-            json: countJson,
-            date: now
+            companyData: countData
         });
     } else {
         console.error('Incorrect value of "count"');
@@ -38,20 +34,19 @@ router.get('/company/:id', function(req, res, next) {
         phoneNumber,
         discription,
         choiceId = util.format(req.params.id);
-    for (var i in json) {
-        if (choiceId == json[i]._id) {
-            companyName = json[i].company;
-            country = json[i].country;
-            foundingDate = json[i].founding_date;
-            phoneNumber = json[i].phone;
-            discription = json[i].discription;
+    for (var i in companyData) {
+        if (choiceId == companyData[i]._id) {
+            companyName = companyData[i].company;
+            country = companyData[i].country;
+            foundingDate = companyData[i].founding_date;
+            phoneNumber = companyData[i].phone;
+            discription = companyData[i].discription;
             res.render('one', {
                 title: companyName,
                 country: country,
                 foundingDate: foundingDate,
                 phoneNumber: phoneNumber,
-                discription: discription,
-                date: now
+                discription: discription
             });
         }
     }
